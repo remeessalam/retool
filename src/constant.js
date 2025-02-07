3040fc1d776ef744823bfca21be7c1b3e0542280
remeessalam <remeesreme4u@gmail.com>
2025-02-07 11:09:16 +0530

    new port folio added

--- src/constant.js
+++ src/constant.js
@@ -44,17 +44,6 @@ import testimonialsuserimage from "./assets/images/testimonialsuserimage.png";
 import contactuspagebanner from "./assets/images/contactuspagebanner.jpg";
 import landingpagebannerapp from "./assets/images/landingpagebannerapp.jpg";
 import landingpagebannerweb from "./assets/images/landingpagebannerweb.png";
-import portfoliwebone from "./assets/images/portfolio/1-min.webp";
-import portfoliwebtwo from "./assets/images/portfolio/2-min.webp";
-import portfoliwebthree from "./assets/images/portfolio/3-min.webp";
-import portfoliwebfour from "./assets/images/portfolio/4-min.webp";
-import portfoliwebfive from "./assets/images/portfolio/5-min.webp";
-import portfoliwebseven from "./assets/images/portfolio/6-min.webp";
-import portfoliappone from "./assets/images/portfolio/1-appmin.webp";
-import portfoliapptwo from "./assets/images/portfolio/2-appmin.webp";
-import portfoliappthree from "./assets/images/portfolio/3-appmin.webp";
-import portfoliappfour from "./assets/images/portfolio/4-appmin.webp";
-import portfolisppsfive from "./assets/images/portfolio/5-appmin.webp";
 import whychooseusimg from "./assets/images/whychooseus.png";
 import ecommerce from "./assets/images/landingpage/ecommerce.png";
 import socialmedia from "./assets/images/landingpage/socialmedia.png";
@@ -80,6 +69,27 @@ import servicepagebanner from "./assets/images/servicepagebanner.jpg";
 import aiImage from "./assets/images/aiimageforcontact.png";
 import landingaboutpc from "./assets/images/landingaboutpc.png";
 import faqbg from "./assets/images/faqbg.png";
+// app porffolio images
+import cryptopadie from "./assets/images/portfolio/app/cryptopadie.png";
+import doctorplus from "./assets/images/portfolio/app/doctorplus.png";
+import dubaitravelguide from "./assets/images/portfolio/app/dubai travel guide.webp";
+import elorahairpalour from "./assets/images/portfolio/app/elora hair palour.webp";
+import evansfrancis from "./assets/images/portfolio/app/evans francis.webp";
+import houseofdeliverance from "./assets/images/portfolio/app/house of deliverance.png";
+import mybitsshop from "./assets/images/portfolio/app/mybitsshop.webp";
+import namaz from "./assets/images/portfolio/app/namaz.webp";
+import potea from "./assets/images/portfolio/app/potea.webp";
+import rentop from "./assets/images/portfolio/app/rentop.png";
+// web portfolio images
+import ghomes from "./assets/images/portfolio/web/5ghomes.webp";
+import Autopilot from "./assets/images/portfolio/web/Autopilot.png";
+import bayut from "./assets/images/portfolio/web/bayut.webp";
+import coldcreekcap from "./assets/images/portfolio/web/cold creekcap.webp";
+import collegenutritionnist from "./assets/images/portfolio/web/collegenutritionnist.png";
+import leadership from "./assets/images/portfolio/web/leadership.net.png";
+import menissacaterings from "./assets/images/portfolio/web/menissa caterings.webp";
+import thinkreality from "./assets/images/portfolio/web/think reality.webp";
+
 export const images = {
   Logo,
   phonenumbersvg,
@@ -581,79 +591,92 @@ export const whyChooseUs = [
 
 export const portfolioDataWeb = [
   {
-    name: "FE Finance",
-    description:
-      "We collaborated with FE Finance to create a user-friendly platform that offers tailored financial solutions, allowing users to access resources and manage their finances effectively. The site features a responsive design and an intuitive interface to enhance user engagement.",
-    image: portfoliwebone,
-    link: "https://www.fe-finance.de",
+    id: 1,
+    name: "5g Homes",
+    image: ghomes,
   },
   {
-    name: "OCXEE",
-    description:
-      "For OCXEE, we developed a global platform that supports students in their international study journey. The website offers a variety of services, from accommodation assistance to banking, providing a seamless experience tailored to users' specific needs.",
-    image: portfoliwebtwo,
-    link: "https://www.ocxee.com",
+    id: 2,
+    name: "Autopilot",
+    image: Autopilot,
   },
   {
-    name: "Leaderbridge",
-    description:
-      "Leaderbridge connects leaders and professionals across industries, fostering collaboration and knowledge sharing. Our team crafted a dynamic platform that emphasizes seamless networking and user experience.",
-    image: portfoliwebthree,
-    link: "https://www.leaderbridge.com",
+    id: 3,
+    name: "Bayut",
+    image: bayut,
   },
   {
-    name: "Gigzio",
-    description:
-      "Gigzio is a job search platform we helped bring to life, designed to connect job seekers with employers efficiently. With a focus on user-centered design, the platform simplifies the job search process and enhances accessibility across devices.",
-    image: portfoliwebfour,
-    link: "http://www.gigzio.com",
-  },
-
-  {
-    name: "FE Group",
-    description:
-      "FE Group's platform offers comprehensive solutions in finance and consultancy. We developed a robust website that reflects the company’s professional approach, enhancing client interaction and support.",
-    image: portfoliwebfive,
-    link: "https://www.fe.group",
+    id: 4,
+    name: "Cold Creekcap",
+    image: coldcreekcap,
   },
   {
-    name: "VTalks",
-    description:
-      "VTalks provides a platform for virtual talks and webinars. We created a highly responsive website to support interactive sessions, allowing users to join, share, and engage with industry experts worldwide.",
-    image: portfoliwebseven,
-    link: "https://www.vtalkz.com",
+    id: 5,
+    name: "College Nutritionnist",
+    image: collegenutritionnist,
+  },
+  {
+    id: 6,
+    name: "Leadership",
+    image: leadership,
+  },
+  {
+    id: 7,
+    name: "Menissa Caterings",
+    image: menissacaterings,
+  },
+  {
+    id: 7,
+    name: "Think Reality",
+    image: thinkreality,
   },
 ];
 
 export const portfolioDataApp = [
   {
-    name: "Partex",
-    description:
-      "We developed the Partex app to streamline logistics operations, ensuring efficient tracking and management of packages. With real-time updates and a user-friendly interface, Partex enhances both operational efficiency and user satisfaction.",
-    image: portfoliappone,
+    id: 1,
+    name: "Cryptopadie",
+    image: cryptopadie,
   },
   {
-    name: "Find A Driver",
-    description:
-      "Driver is a comprehensive driver management platform that simplifies fleet operations, driver scheduling, and route optimization. The app is designed to maximize productivity and provide seamless navigation features.",
-    image: portfoliapptwo,
+    id: 2,
+    name: "Doctorplus",
+    image: doctorplus,
   },
   {
-    name: "Glowsy",
-    description:
-      "Glowsy is a beauty and wellness app offering personalized beauty care recommendations and virtual consultations. Our team developed this app to provide an engaging, user-centered experience with tailored beauty solutions.",
-    image: portfoliappthree,
+    id: 3,
+    name: "Dubai Travel Guide",
+    image: dubaitravelguide,
+  },
+  { id: 4, name: "Elora Hair Palour", image: elorahairpalour },
+  {
+    id: 5,
+    name: "Evans Francis",
+    image: evansfrancis,
   },
   {
-    name: "Artisan Express",
-    description:
-      "Artisan Express connects skilled artisans with clients, offering a platform for job booking, ratings, and secure payments. The app enhances user trust and accessibility, creating a dynamic marketplace for craft and trade services.",
-    image: portfoliappfour,
+    id: 6,
+    name: "House Of Deliverance",
+    image: houseofdeliverance,
   },
   {
-    name: "House of Deliverance",
-    description:
-      "House of Deliverance is a wellness app designed to provide mental health resources, guided meditation, and daily motivational content. The app fosters a supportive community and offers tools for users to improve mental well-being.",
-    image: portfolisppsfive,
+    id: 7,
+    name: "Mybitsshop",
+    image: mybitsshop,
+  },
+  {
+    id: 8,
+    name: "Namaz",
+    image: namaz,
+  },
+  {
+    id: 9,
+    name: "Potea",
+    image: potea,
+  },
+  {
+    id: 9,
+    name: "Rentop",
+    image: rentop,
   },
 ];
